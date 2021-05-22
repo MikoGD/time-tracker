66655f618ff7d3a74ac4f969914cd1ac5e069d9f
Mikael Escolin <mikaelescolin@gmail.com>
2021-05-22 20:16:10 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const App: React.FunctionComponent = () => {
+  return (
+    <div className="App">
+      <h2>Hello, World!</h2>
+    </div>
+  );
+};
+
+export default App;
