40fce16f801ab517e04398c0d2d984be09458b6d
Mikael Escolin <mikaelescolin@gmail.com>
2021-05-22 20:30:35 +0100

    Add chakra-ui and font

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme';
 
 const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <h2>Hello, World!</h2>
-    </div>
+    <ChakraProvider theme={theme}>
+      <div className="App">
+        <h2>Hello, World!</h2>
+      </div>
+    </ChakraProvider>
   );
 };
 
