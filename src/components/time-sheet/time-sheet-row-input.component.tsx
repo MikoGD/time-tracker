ccbfddc4e86984e7b4073ee2ff5658f7f6010dad
Mikael Escolin <mikaelescolin@gmail.com>
2021-05-23 11:24:47 +0100

    Add select to start and end time inputs
    - Installed react-select as I needed to filter from typing as well
    - Added memo for times in intervals of 5 minutes
    - Added memo for options

--- src/components/time-sheet/time-sheet-row-input.component.tsx
+++ src/components/time-sheet/time-sheet-row-input.component.tsx
@@ -1,10 +1,42 @@
-import React from 'react';
-import { Grid, Flex, FormControl, FormLabel, Input } from '@chakra-ui/react';
+import React, { useMemo } from 'react';
+import Select from 'react-select';
+import { Flex, FormControl, FormLabel, Grid, Input } from '@chakra-ui/react';
 
 // REVIEW:
 interface TimeSheetRowInputProps {}
 
+const padLeadingZeros = (num: string, amount: number) => {
+  let newNum = num;
+  while (newNum.length < amount) newNum = `${0}${num}`;
+
+  return newNum;
+};
+
+interface TimeOption {
+  value: string;
+  label: string;
+}
+
 export const TimeSheetRowInput: React.FC<TimeSheetRowInputProps> = () => {
+  const times = useMemo(() => {
+    const tempTimes: string[] = [];
+
+    for (let hour = 0; hour <= 24; hour += 1) {
+      for (let minute = 0; minute < 60; minute += 5) {
+        tempTimes.push(`${padLeadingZeros(`${hour}`, 2)}:${padLeadingZeros(`${minute}`, 2)}`);
+      }
+    }
+    return tempTimes;
+  }, []);
+
+  const options = useMemo(() => {
+    const tempOptions: TimeOption[] = [];
+    times.forEach((time) => {
+      tempOptions.push({ label: time, value: time });
+    });
+    return tempOptions;
+  }, []);
+
   return (
     <Grid templateColumns="3fr 2fr 2fr 1.75fr" w="100%" borderBottom="1px solid black" h="10rem">
       <FormControl as={Flex} id="description" flexDir="column">
@@ -13,11 +45,11 @@ export const TimeSheetRowInput: React.FC<TimeSheetRowInputProps> = () => {
       </FormControl>
       <FormControl as={Flex} id="start-time" flexDir="column">
         <FormLabel>Start time</FormLabel>
-        <Input borderRadius="none" />
+        <Select options={options} />
       </FormControl>
       <FormControl as={Flex} id="end-time" flexDir="column">
         <FormLabel>End time</FormLabel>
-        <Input borderRadius="none" />
+        <Select options={options} />
       </FormControl>
       <FormControl as={Flex} id="elapsed-time" flexDir="column">
         <FormLabel>Elapsed time</FormLabel>
