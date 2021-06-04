878cb9df40006380e4964b4633f861b0be1a659d
Mikael Escolin <mikaelescolin@gmail.com>
2021-06-04 22:49:46 +0100

    Update configured-field to allow textarea and styling

--- src/components/form/configured-field.component.tsx
+++ src/components/form/configured-field.component.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Input, InputProps } from '@chakra-ui/react';
+import { Input, InputProps, Textarea } from '@chakra-ui/react';
 import { Field } from 'formik';
+import { checkStyles, Styles } from '../../utility';
 
 interface ConfiguredFieldProps extends InputProps {
   name: string;
   id: string;
+  styles?: Styles;
+  textArea?: boolean;
 }
 
-export const ConfiguredField: React.FC<ConfiguredFieldProps> = ({ name, id, ...rest }) => {
-  return <Field name={name} id={id} as={Input} {...rest} />;
+export const ConfiguredField: React.FC<ConfiguredFieldProps> = ({ name, id, styles, textArea, ...rest }) => {
+  const allowedStyles = ['h'];
+  checkStyles(styles, name, allowedStyles);
+
+  return <Field name={name} id={id} as={textArea ? Textarea : Input} {...styles} {...rest} />;
 };
