b8839b94c0dcfa0815b1f5709e4efdddcb2906f2
Mikael Escolin <mikaelescolin@gmail.com>
2021-06-04 22:53:32 +0100

    Update time-sheet
    - Refactored time-sheet-row-input to time-sheet-input
    - time-sheet-input is not a form on the side of the time-sheet and not a
      row
    - Moved configured-form to time-sheet-input

--- src/components/time-sheet/time-sheet.container.tsx
+++ src/components/time-sheet/time-sheet.container.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import { FormikProps } from 'formik';
-import { ConfiguredForm } from '../form';
 import { TimeSheetView } from './time-sheet.view';
-import { TimeSheetSchema } from './time-sheet.model';
 
 // REVIEW:
 // interface TimeSheetContainerProps {}
 
 export const TimeSheetContainer: React.FC = () => {
-  const initialValues: TimeSheetSchema = {
-    description: '',
-    startTime: '09:00',
-    endTime: '10:00',
-    elapsedTime: '1h',
-  };
-  return (
-    <ConfiguredForm<TimeSheetSchema> initialValues={initialValues}>
-      {(formikBag: FormikProps<TimeSheetSchema>) => <TimeSheetView formikBag={formikBag} />}
-    </ConfiguredForm>
-  );
+  return <TimeSheetView />;
 };
 
 export const TimeSheet = TimeSheetContainer;
--- src/components/time-sheet/time-sheet.view.tsx
+++ src/components/time-sheet/time-sheet.view.tsx
@@ -1,33 +1,30 @@
 import React from 'react';
-import { FormikProps } from 'formik';
-import { VStack } from '@chakra-ui/react';
+import { Grid, VStack } from '@chakra-ui/react';
 import { TimeSheetRow } from './time-sheet-row.component';
-import { TimeSheetRowInput } from './time-sheet-row-input.component';
-import { TimeSheetSchema } from './time-sheet.model';
+import { TimeSheetInput } from './time-sheet-input.component';
 
 // REVIEW:
-interface TimeSheetViewProps {
-  formikBag: FormikProps<TimeSheetSchema>;
-}
 
-export const TimeSheetView: React.FC<TimeSheetViewProps> = ({ formikBag }) => {
+export const TimeSheetView: React.FC = () => {
   const amount: number[] = [];
   for (let i = 0; i < 50; i += 1) {
     amount.push(i);
   }
   return (
-    <VStack>
-      <TimeSheetRow isHeader />
-      <VStack w="100%" maxH="70vh" overflow="auto" spacing="none">
-        {amount.map((elem) => {
-          if (elem % 2 === 0) {
-            return <TimeSheetRow key={Math.random()} styles={{ bgColor: 'gray.200' }} />;
-          }
+    <Grid templateColumns="4fr 1fr" columnGap="5">
+      <VStack>
+        <TimeSheetRow isHeader />
+        <VStack w="100%" maxH="70vh" overflow="auto" spacing="none">
+          {amount.map((elem) => {
+            if (elem % 2 === 0) {
+              return <TimeSheetRow key={Math.random()} styles={{ bgColor: 'gray.200' }} />;
+            }
 
-          return <TimeSheetRow key={Math.random()} styles={{ bgColor: 'gray.50' }} />;
-        })}
+            return <TimeSheetRow key={Math.random()} styles={{ bgColor: 'gray.50' }} />;
+          })}
+        </VStack>
       </VStack>
-      <TimeSheetRowInput formikBag={formikBag} />
-    </VStack>
+      <TimeSheetInput />
+    </Grid>
   );
 };
